9662df3ba24fcb43a49cdd066aab78350e011c53
Dzaky Purnomo Rifai <76737872+dzaky-pr@users.noreply.github.com>
2026-06-12 08:51:23 +0000

    feat: create action.yml Dockerfile and entrypoint.sh to setup and prepare publish to public

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "=============================="
+
+git config --global user.name "{$GITHUB_ACTOR}"
+git config --global user.email "{$INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "feat: Update Feed"
+git push --set-up stream origin main
+
+echo "=============================="
